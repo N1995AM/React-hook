c0764b72abd1fbece59e678f475857f9ed295dcf
unknown <andreiv0202@gmail.com>
2021-01-19 06:28:24 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useReducer, useEffect, useState } from "react"; 
+
+function reducer(state, action) {
+  switch (action.type) {
+      case 'INCREASE':
+      return { value: state.value + 1 };
+      case 'DECREASE':
+      return { value: state.value - 1 };
+      default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="App">
+      <p> Current Counter Value : {state.value}</p>
+      <button onClick={() => dispatch({ type: 'INCREASE' })}>+1</button>
+      <button onClick={() => dispatch({ type: 'DECREASE' })}>-1</button>
+      </div>
   );
-}
+};
 
 export default App;
